d272a5e1553536e2caaa7c18be0ee5e1405fa621
aufarabul <aufarabul@gmail.com>
2024-04-26 17:58:39 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import { ToastContainer } from "react-toastify";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+import "bootstrap/dist/css/bootstrap.min.css"; // apply bootstrap for styling
+import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Container className="mt-5">
+          <Home />
+        </Container>
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <>
+        <Navbar />
+        <Container className="mt-5">
+          <Login />
+        </Container>
+      </>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <>
+        <Navbar />
+        <Container className="mt-5">
+          <Register />
+        </Container>
+      </>
+    ),
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+
+      <ToastContainer theme="colored" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const NavbarComponent = () => {
+  const [user, setUser] = useState(null);
+
+  const getProfile = async (token) => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${import.meta.env.VITE_BACKEND_API}/api/auth/profile`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    try {
+      const response = await axios.request(config);
+      const { data } = response.data;
+
+      // set user by response
+      setUser(data);
+    } catch (error) {
+      // because token is not valid, we will delete it from local storage
+      setUser(null);
+      localStorage.removeItem("token");
+    }
+  };
+
+  useEffect(() => {
+    // get user profile if we have token
+    const token = localStorage.getItem("token");
+    if (token) {
+      getProfile(token);
+    }
+  }, []);
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Kampus Merdeka
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            {user ? (
+              <>
+                <Nav.Link as={Link} to="/profile">
+                  {user?.name}
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    window.location = "";
+                  }}
+                >
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
